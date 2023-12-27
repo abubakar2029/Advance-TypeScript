5f0f98dfab802769ab719f6c016a698ecb9688a3
Abu Bakar <abubakarzafar2029@gmail.com>
2023-12-27 23:26:30 +0500

    Enums and tuples.ts added

---
+++ 1 Fundamentals/enums.ts
@@ -0,0 +1,18 @@
+// To represent a list of related constants
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+// PascalCase
+const enum Size {
+  Small,
+  Medium,
+  Large,
+}
+// By default values will be assigned starting from 0
+
+let mySize: Size = Size.Medium;
+
+// In most cases we set Explicit Values
+ 
